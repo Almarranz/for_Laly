f00f2e64030fef888ba1fcb4e4309c5daace8c99
Alvaro Martinez <amartinez@iaa.es>
2024-10-21 11:17:41 +0200

    Alignmet in Loop.It does not converge

--- pm.py
+++ pm.py
@@ -40,98 +40,63 @@ l2_xy = np.array([l2['x'],l2['y']]).T
 
 d_m = 1
 deg = 1
-sig_cl = 3
-comp = compare_lists(l1_xy,l2_xy,d_m)
+sig_cl = 2
+
 
 
 # comp_t = Table(comp, names =('l1_x','l1_y','l2_x','l2_y','ind_1','ind_2','dist'))
 
-l1_com = l1[comp['ind_1']]
-l2_com = l2[comp['ind_2']]
-
-diff_mag = l1_com['IB224_diff'] - l2_com['IB224_diff'] 
-diff_mag1 = l1_com['IB230_diff'] - l2_com['IB230_diff'] 
-
-
-mask_m, l_lim,h_lim = sigma_clip(diff_mag, sigma=sig_cl, masked = True, return_bounds= True)
-
-l1_clip = l1_com[~mask_m.mask]
-l2_clip = l2_com[~mask_m.mask]
-
-fig, (ax,ax1) = plt.subplots(1,2)
-ax.set_xlabel('$\Delta$ IB224')
-ax1.set_xlabel('$\Delta$ IB230')
-
-ax.hist(diff_mag, label = 'matches = %s\ndist = %.2f arcsec'%(len(comp['ind_1']), d_m*0.0135))
-ax1.hist(diff_mag1)
-
-ax.axvline(l_lim, color = 'red', ls = 'dashed', label = '$\pm$%s$\sigma$'%(sig_cl))
-ax.axvline(h_lim, color = 'red', ls = 'dashed')
-
-ax.legend()
-ax1.legend()
-
-
-
-
-xy_1c = np.array([l1_clip['x'],l1_clip['y']]).T
-xy_2c = np.array([l2_clip['x'],l2_clip['y']]).T
-p = ski.transform.estimate_transform('polynomial',
-                                      xy_1c, 
-                                      xy_2c, order = deg)
-# p = ski.transform.estimate_transform('similarity',
-#                                      xy_1, 
-#                                      xy_2)
-
-l1_xyt = p(l1_xy)
-
-comp_i = compare_lists(l1_xyt,l2_xy,d_m)
-
-print(len(comp),len(comp_i))
-
-
-l1_com = l1[comp_i['ind_1']]
-l2_com = l2[comp_i['ind_2']]
-
-diff_mag = l1_com['IB224_diff'] - l2_com['IB224_diff'] 
-diff_mag1 = l1_com['IB230_diff'] - l2_com['IB230_diff'] 
-
-
-mask_m, l_lim,h_lim = sigma_clip(diff_mag, sigma=sig_cl, masked = True, return_bounds= True)
-
-l1_clip = l1_com[~mask_m.mask]
-l2_clip = l2_com[~mask_m.mask]
-
-fig, (ax,ax1) = plt.subplots(1,2)
-ax.set_xlabel('$\Delta$ IB224')
-ax1.set_xlabel('$\Delta$ IB230')
-
-ax.hist(diff_mag, label = 'matches = %s\ndist = %.2f arcsec'%(len(comp_i['ind_1']), d_m*0.0135))
-ax1.hist(diff_mag1)
-
-ax.axvline(l_lim, color = 'red', ls = 'dashed', label = '$\pm$%s$\sigma$'%(sig_cl))
-ax.axvline(h_lim, color = 'red', ls = 'dashed')
-
-ax.legend()
-ax1.legend()
-
-
-
-xy_1c = np.array([l1_clip['x'],l1_clip['y']]).T
-xy_2c = np.array([l2_clip['x'],l2_clip['y']]).T
-p2 = ski.transform.estimate_transform('polynomial',
-                                      xy_1c, 
-                                      xy_2c, order = deg)
-# p = ski.transform.estimate_transform('similarity',
-#                                      xy_1, 
-#                                      xy_2)
-
-l1_xytt = p2(l1_xyt)
-
-
-comp_ii = compare_lists(l1_xytt,l2_xy,d_m)
-
-print(len(comp_i),len(comp_ii))
+for loop in range(2):
+    
+    comp = compare_lists(l1_xy,l2_xy,d_m)
+    print(f'Common in loop {loop} = %s'%(len(comp['ind_1'])))
+    l1_com = l1[comp['ind_1']]
+    l2_com = l2[comp['ind_2']]
+    
+    diff_mag = l1_com['IB224_diff'] - l2_com['IB224_diff'] 
+    diff_mag1 = l1_com['IB230_diff'] - l2_com['IB230_diff'] 
+    
+    
+    mask_m, l_lim,h_lim = sigma_clip(diff_mag, sigma=sig_cl, masked = True, return_bounds= True)
+    
+    
+    
+    l2_clip = l2_com
+    l1_clip = l1_com
+    
+# =============================================================================
+#     l1_clip = l1_com[~mask_m.mask]
+#     l2_clip = l2_com[~mask_m.mask]
+#     
+#     fig, (ax,ax1) = plt.subplots(1,2)
+#     ax.set_xlabel('$\Delta$ IB224')
+#     ax1.set_xlabel('$\Delta$ IB230')
+#     
+#     ax.hist(diff_mag, label = 'matches = %s\ndist = %.2f arcsec'%(len(comp['ind_1']), d_m*0.0135))
+#     ax1.hist(diff_mag1)
+#     
+#     ax.axvline(l_lim, color = 'red', ls = 'dashed', label = '$\pm$%s$\sigma$'%(sig_cl))
+#     ax.axvline(h_lim, color = 'red', ls = 'dashed')
+#     
+#     ax.legend()
+# =============================================================================
+    # ax1.legend()
+    
+    
+    
+    
+    xy_1c = np.array([l1_clip['x'],l1_clip['y']]).T
+    xy_2c = np.array([l2_clip['x'],l2_clip['y']]).T
+    p = ski.transform.estimate_transform('polynomial',
+                                          xy_1c, 
+                                          xy_2c, order = deg)
+    # p = ski.transform.estimate_transform('similarity',
+    #                                       xy_1c, 
+    #                                       xy_2c)
+    
+    l1_xy = p(l1_xy)
+    
+  
 
 
 
